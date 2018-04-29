d813eaf04798952b742a2438eb2e391e55e16926
Dalton Hill <dalton.hill2022@gmail.com>
2018-04-29 16:25:03 -0500

    Cleaned up the 'Gathering' component.

--- src/components/TreePlanner/Gathering/Body.js
+++ src/components/TreePlanner/Gathering/Body.js
@@ -6,8 +6,19 @@ const body = ({ trees, handleLogCountChange }) => {
   return (
     <div>
       <h5 className="card-title">Gathering</h5>
-      { trees.map((tree, index) => <Log className={"row"} log={tree.log}
-                                        change={handleLogCountChange} key={index}/>) }
+      <table className={"table"}>
+        <thead>
+          <tr>
+            <th>Type</th>
+            <th>Gathered</th>
+            <th>Experience Gained</th>
+          </tr>
+        </thead>
+        <tbody>
+          { trees.map((tree, index) => <Log className={"row"} log={tree.log}
+                                            change={handleLogCountChange} key={index}/>) }
+        </tbody>
+      </table>
     </div>
   )
 };
--- src/components/TreePlanner/Gathering/Log/Log.js
+++ src/components/TreePlanner/Gathering/Log/Log.js
@@ -6,23 +6,11 @@ const log = ({ log, change }) => {
   const isCountValid = !isNaN(parseInt(log.count, 10));
   const experienceGained = isCountValid ? parseInt(log.count, 10) * log.woodcutting_xp : 0;
   return (
-    <div className={["form-row", css["log-row"]].join(' ')}>
-      <div className={"col-mid-3 mb-3"}>
-        <img src={log.image_url} alt={log.name} className={css["log-image"]}/>
-      </div>
-      <div className={"col-mid-3 mb-3"}>
-        <p>{log.name}</p>
-      </div>
-      <div className={"col-mid-3 mb-3"}>
-        <label>Count Chopped</label>
-        <input value={log.count} className={"form-control"}
-               onChange={(event) => change(event, log.name)}/>
-      </div>
-      <div className={"col-mid-3 mb-3"}>
-        <label>Experience Gained</label>
-        <input className={"form-control"} readOnly={true} value={experienceGained}/>
-      </div>
-    </div>
+    <tr>
+      <td><img src={log.image_url} alt={log.name} className={css["log-image"]}/><p>{log.name}</p></td>
+      <td><input value={log.count} className={"form-control"} onChange={(event) => change(event, log.name)}/></td>
+      <td><input className={"form-control"} readOnly={true} value={experienceGained}/></td>
+    </tr>
   );
 };
 
