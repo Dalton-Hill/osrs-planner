e34638f1c3b0a55f13ca300f118370e6d932ca24
Dalton Hill <dalton.hill2022@gmail.com>
2018-05-14 09:05:51 -0500

    Made slight improvements to navbar that take full advantage of bootstrap built-ins.

--- src/Navigation/Navbar/Navbar.js
+++ src/Navigation/Navbar/Navbar.js
@@ -5,8 +5,13 @@ import { Link } from 'react-router-dom';
 const navbar = (props) => {
   return (
     <nav className={"navbar navbar-expand-lg navbar-dark bg-dark"}>
-      <div className={"collapse navbar-collapse"}>
-        <ul className={"navbar-nav mr-auto"}>
+      <Link className="navbar-brand" to={"/Home"}>IronCalculator</Link>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse"
+              aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"/>
+      </button>
+      <div className={"collapse navbar-collapse"} id={"navbarCollapse"}>
+        <ul className={"navbar-nav"}>
           {props.routes.map((route, index) => {
             return <li className={"nav-item"} key={index}><Link className={"nav-link"} to={route.description}>{route.description}</Link></li>
           })}
