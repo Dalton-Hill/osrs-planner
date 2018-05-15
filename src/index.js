389923d5bfcfa75a3e549d488df09f489386ccdf
Dalton Hill <dalton.hill2022@gmail.com>
2018-05-15 15:35:12 -0500

    No current need for redux to be added to this project. Removed files and uninstalled packages.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-
-import reducer from './containers/TreePlanner/store/reducer';
 import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 
 
-const store = createStore(reducer);
-
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
