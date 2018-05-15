b9eb73c2f01315a20740e7b315fcbeb4732449c6
Dalton Hill <dalton.hill2022@gmail.com>
2018-05-15 09:23:07 -0500

    navbar now shows the active link.

--- src/Navigation/Navbar/Navbar.js
+++ src/Navigation/Navbar/Navbar.js
@@ -1,25 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 
-const navbar = (props) => {
-  return (
-    <nav className={"navbar navbar-expand-lg navbar-dark bg-dark"}>
-      <Link className="navbar-brand" to={"/Home"}>IronCalculator</Link>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse"
-              aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"/>
-      </button>
-      <div className={"collapse navbar-collapse"} id={"navbarCollapse"}>
-        <ul className={"navbar-nav"}>
-          {props.routes.map((route, index) => {
-            return <li className={"nav-item"} key={index}><Link className={"nav-link"} to={route.description}>{route.description}</Link></li>
-          })}
-        </ul>
-      </div>
-    </nav>
-  )
-};
+class navbar extends Component {
+  state = {
+    activeLinkIndex: null
+  };
+
+  linkClickHandler = (indexOfLinkClicked) => {
+    this.setState({ activeLinkIndex: indexOfLinkClicked })
+  };
+
+  render() {
+    return (
+      <nav className={"navbar navbar-expand-lg navbar-dark bg-dark"}>
+        <Link className="navbar-brand" to={"/Home"} onClick={() => this.setState({ activeLinkIndex: null })}>IronCalculator</Link>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse"
+                aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"/>
+        </button>
+        <div className={"collapse navbar-collapse"} id={"navbarCollapse"}>
+          <ul className={"navbar-nav"}>
+            {this.props.routes.map((route, index) => {
+              const linkClass = index === this.state.activeLinkIndex ? "nav-link active" : "nav-link";
+              return <li className={"nav-item"} key={index}>
+                <Link className={linkClass} to={route.description} onClick={() => this.linkClickHandler(index)}
+                >{route.description}</Link></li>
+            })}
+          </ul>
+        </div>
+      </nav>
+    )
+  }
+}
 
 
 export default navbar;
