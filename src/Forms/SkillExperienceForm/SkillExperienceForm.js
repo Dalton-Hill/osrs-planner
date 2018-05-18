f490beff3152fd9eb8b57e6ec5e673910747b6d0
Dalton Hill <dalton.hill2022@gmail.com>
2018-05-17 19:47:30 -0500

    Ran into multiple problems with the comma issue. Attempted to use outside solutions as well as my own. There seems to be an issue here that I can't identify at the moment. Don't care enough about the commas to continue down this rabbit hole.

--- src/Forms/SkillExperienceForm/SkillExperienceForm.js
+++ src/Forms/SkillExperienceForm/SkillExperienceForm.js
@@ -2,33 +2,12 @@ import React from 'react';
 import * as styles from './styles';
 
 
-const formatNumber = (number) => {
-  let strNumber = number.toString();
-  if (isNaN(strNumber)) return strNumber;
-
-  const indexOfDecimal = strNumber.indexOf('.');
-  const wholeNumber = indexOfDecimal === -1 ? strNumber : strNumber.slice(0, indexOfDecimal);
-  let wholeNumArray = wholeNumber.split('');
-  console.log(placeCommas(wholeNumArray, wholeNumArray.length - 1));
-  return strNumber;
-};
-
-
-const placeCommas = (str, currentPosition) => {
-  const length = str.join('').replace(',', '').length;
-  if (currentPosition === 0) return str;
-  if ((currentPosition - length) % 3 === 0) {
-    return placeCommas([...str.splice(0, currentPosition), ',', ...str.splice(currentPosition)], currentPosition - 1)
-  }
-};
-
-
 const skillExperienceForm = (props) => {
   return (
     <form style={styles.form}>
       <div className="form-group">
         <label htmlFor={"startingXPInput"}>Starting XP</label>
-        <input id={"startingXPInput"} type="text" className="form-control" value={formatNumber(props.startingXP)}
+        <input id={"startingXPInput"} type="text" className="form-control" value={props.startingXP}
         onChange={(event) => props.onChangeStartingXP(event)}/>
       </div>
       <div className="form-group">
