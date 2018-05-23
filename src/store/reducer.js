0269f101e50014186a5712b78866296909ef713f
Dalton Hill <dalton.hill2022@gmail.com>
2018-05-23 08:12:54 -0500

    added firemaking info to the initialState.

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -9,7 +9,7 @@ const initialState = {
       counts: [
         {location: 'inventory', count: 0, isPositive: true, imageName: "Inventory.png"},
         {location: 'woodcutting', count: 0, isPositive: true, xpPer: 100, levelRequired: 45, imageName: "woodcutting_icon.png"},
-        {location: 'firemaking', count: 0, isPositive: false, imageName: "Firemaking_icon.png"},
+        {location: 'firemaking', count: 0, isPositive: false, xpPer: 135, levelRequired: 45, imageName: "Firemaking_icon.png"},
         {location: 'fletching', count: 0, isPositive: false, imageName: "Fletching_icon.png"},
       ]
     },
@@ -20,7 +20,7 @@ const initialState = {
       counts: [
         {location: 'inventory', count: 0, isPositive: true, imageName: "Inventory.png"},
         {location: 'woodcutting', count: 0, isPositive: true, xpPer: 175, levelRequired: 60, imageName: "woodcutting_icon.png"},
-        {location: 'firemaking', count: 0, isPositive: false, imageName: "Firemaking_icon.png"},
+        {location: 'firemaking', count: 0, isPositive: false, xpPer: 202.5, levelRequired: 60, imageName: "Firemaking_icon.png"},
         {location: 'fletching', count: 0, isPositive: false, imageName: "Fletching_icon.png"},
       ]
     },
@@ -31,7 +31,7 @@ const initialState = {
       counts: [
         {location: 'inventory', count: 0, isPositive: true, imageName: "Inventory.png"},
         {location: 'woodcutting', count: 0, isPositive: true, xpPer: 250, levelRequired: 75, imageName: "woodcutting_icon.png"},
-        {location: 'firemaking', count: 0, isPositive: false, imageName: "Firemaking_icon.png"},
+        {location: 'firemaking', count: 0, isPositive: false, xpPer: 303.8 , levelRequired: 75, imageName: "Firemaking_icon.png"},
         {location: 'fletching', count: 0, isPositive: false, imageName: "Fletching_icon.png"},
       ]
     }
