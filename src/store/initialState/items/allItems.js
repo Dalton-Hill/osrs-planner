b4a56a9e410648be56f16964f45641b53ef081fc
Dalton Hill <dalton.hill2022@gmail.com>
2018-05-28 12:03:41 -0500

    Added all clean herbs to the items store.

--- src/store/initialState/items/allItems.js
+++ src/store/initialState/items/allItems.js
@@ -1,6 +1,7 @@
 import fletchingProducts from './types/fletchingProducts';
 import fletchingSecondaries from './types/fletchingSecondaries';
 import grimyHerbs from './types/grimyHerbs';
+import cleanHerbs from './types/cleanHerbs';
 import logs from './types/logs';
 import misc from './types/misc';
 
@@ -9,6 +10,7 @@ const allItems = [
   ...fletchingSecondaries,
   ...fletchingProducts,
   ...grimyHerbs,
+  ...cleanHerbs,
   ...logs,
   ...misc,
 ];
---
+++ src/store/initialState/items/types/cleanHerbs.js
@@ -0,0 +1,108 @@
+import { cleanHerb } from "../allTypeNames";
+import {
+  avantoe,
+  cadantine,
+  dwarfWeed,
+  guam,
+  harralander,
+  irit,
+  kwuarm,
+  lantadyme,
+  marrentill,
+  ranarrWeed,
+  snapdragon,
+  tarromin,
+  toadflax,
+  torstol
+} from "../allItemNames";
+
+
+const cleanHerbs = [
+  {
+    name: avantoe,
+    imageName: 'Avantoe.png',
+    type: cleanHerb,
+    count: 0
+  },
+  {
+    name: cadantine,
+    imageName: 'Cadantine.png',
+    type: cleanHerb,
+    count: 0
+  },
+  {
+    name: dwarfWeed,
+    imageName: 'Dwarf_weed.png',
+    type: cleanHerb,
+    count: 0
+  },
+  {
+    name: guam,
+    imageName: 'Guam_leaf.png',
+    type: cleanHerb,
+    count: 0
+  },
+  {
+    name: harralander,
+    imageName: 'Harralander.png',
+    type: cleanHerb,
+    count: 0
+  },
+  {
+    name: irit,
+    imageName: 'Irit_leaf.png',
+    type: cleanHerb,
+    count: 0
+  },
+  {
+    name: kwuarm,
+    imageName: 'Kwuarm.png',
+    type: cleanHerb,
+    count: 0
+  },
+  {
+    name: lantadyme,
+    imageName: 'Lantadyme.png',
+    type: cleanHerb,
+    count: 0
+  },
+  {
+    name: marrentill,
+    imageName: 'Marrentill.png',
+    type: cleanHerb,
+    count: 0
+  },
+  {
+    name: ranarrWeed,
+    imageName: 'Ranarr_weed.png',
+    type: cleanHerb,
+    count: 0
+  },
+  {
+    name: snapdragon,
+    imageName: 'Snapdragon.png',
+    type: cleanHerb,
+    count: 0
+  },
+  {
+    name: tarromin,
+    imageName: 'Tarromin.png',
+    type: cleanHerb,
+    count: 0
+  },
+  {
+    name: toadflax,
+    imageName: 'Toadflax.png',
+    type: cleanHerb,
+    count: 0
+  },
+  {
+    name: torstol,
+    imageName: 'Torstol.png',
+    type: cleanHerb,
+    count: 0
+  },
+];
+
+
+export default cleanHerbs;
