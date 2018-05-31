b614936e46187c6e476f32dd1530b8217390f0d3
Dalton Hill <dalton.hill2022@gmail.com>
2018-05-31 11:42:23 -0500

    Removed tabindex from inventory icons. Added 'title' to inventory icons.

--- src/components/Inventory/InventorySection/Table/Row/UsageButton/UsageButton.js
+++ src/components/Inventory/InventorySection/Table/Row/UsageButton/UsageButton.js
@@ -5,7 +5,7 @@ import * as styles from "./styles";
 const usageButton = (props) => {
   const badgeClass = props.isPositive ? "badge badge-success" : "badge badge-danger";
   return (
-    <button className={"btn btn-outline-info"}>
+    <button className={"btn btn-outline-info"} tabIndex={-1} title={props.skill.name}>
       <img src={require('../../../../../../Assets/images/' + props.skill.imageName)} alt={props.skill.name}
            style={styles.usageButtonImage}/>
       <span className={badgeClass} style={styles.badge}>{props.skill.count}</span>
