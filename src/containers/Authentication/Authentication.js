70353049f75389ae4b1eee43084cc7b338a5ad39
Dalton Hill <dalton.hill2022@gmail.com>
2018-06-08 14:23:01 -0500

    Authentication Section working (tab navigation/overall layout) Need to create SignIn and ConfirmEmail components.

--- src/containers/Authentication/Authentication.js
+++ src/containers/Authentication/Authentication.js
@@ -1,60 +1,37 @@
 import React, { Component } from 'react';
-import { Auth } from 'aws-amplify';
+import SignUp from "../../Forms/SignUp/SignUp";
 
 
 class Authentication extends Component {
   state = {
-    username: '',
-    password: '',
+    activeSectionIndex: 0,
+    sections: [
+      {title: 'Sign in', comp: SignUp},
+      {title: 'Sign up', comp: SignUp},
+      {title: 'Confirm email', comp: SignUp},
+    ]
   };
 
-  handleSubmit = (event) => {
-    event.preventDefault();
-    Auth.signUp(this.state)
-      .then(data => console.log(data))
-      .catch(err => console.log(err));
-  };
-
-  handleEmailChange = (event) => {
-    this.setState({ username: event.target.value })
-  };
-
-  handlePasswordChange = (event) => {
-    this.setState({ password: event.target.value })
+  handleTabClick = (index) => {
+    this.setState({ activeSectionIndex: index })
   };
 
   render() {
+    const ActiveComponent = this.state.sections[this.state.activeSectionIndex].comp;
     return (
       <div className={"card"}>
         <div className={"card-header"}>
-          Authentication
+          <ul className="nav nav-tabs card-header-tabs">
+            {this.state.sections.map((section, index) => {
+              const navLinkClass = this.state.activeSectionIndex === index ? 'nav-link active' : 'nav-link';
+              return <li className={"nav-item"} key={index}>
+                <a className={navLinkClass} onClick={() => this.handleTabClick(index)} style={{cursor: "pointer"}}>{section.title}</a>
+              </li>
+            })}
+          </ul>
         </div>
         <div className={"card-body"}>
-          <form>
-            <div className="form-group">
-              <label htmlFor="exampleInputEmail1">Email address</label>
-              <input
-                onChange={this.handleEmailChange}
-                type="email"
-                className="form-control"
-                id="exampleInputEmail1"
-                aria-describedby="emailHelp"
-                placeholder="Enter email"/>
-              <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.
-              </small>
-            </div>
-
-            <div className="form-group">
-              <label htmlFor="exampleInputPassword1">Password</label>
-              <input
-                onChange={this.handlePasswordChange}
-                type="password"
-                className="form-control"
-                id="exampleInputPassword1"
-                placeholder="Password"/>
-            </div>
-            <button className="btn btn-primary" onClick={this.handleSubmit}>Submit</button>
-          </form>
+          <ActiveComponent/>
         </div>
       </div>
     )
